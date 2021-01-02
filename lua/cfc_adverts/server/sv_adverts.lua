8d92d649c01854196137433b503a0a8554da334d
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2021-01-02 18:52:21 -0500

    Transfer from old repo

---
+++ lua/cfc_adverts/server/sv_adverts.lua
@@ -0,0 +1,73 @@
+CFCAdverts = CFCAdverts or {}
+
+local advertCount = 0
+local curAdvert = 1
+local advertKeys = {}
+local timerName = CFCAdverts.HOOK_BASE .. "_SendAdvert"
+
+function CFCAdverts.updateAdverts()
+    advertCount = table.Count( CFCAdverts.adverts )
+    advertKeys = table.GetKeys( CFCAdverts.adverts )
+    
+    if advertCount == 0 then
+        timer.Simple( 30, CFCAdverts.updateAdverts ) --Prevent CFC Adverts from dying forever if the list goes empty
+        return
+    end
+    
+    curAdvert = math.min( curAdvert, advertCount )
+    local advertGap = CFCAdverts.cycleTime / advertCount
+    
+    timer.Create( timerName, advertGap, 0, function()
+        local plys = player.GetHumans()
+        
+        if #plys == 0 then return end
+        
+        local advertName = advertKeys[curAdvert] or ""
+        local advert = CFCAdverts.adverts[advertName] or {}
+        
+        if table.IsEmpty( advert ) or table.Count( CFCAdverts.adverts ) ~= advertCount then
+            timer.Remove( timerName ) --Redundant, but for just in case
+            CFCAdverts.updateAdverts()
+            return
+        end
+        
+        local notifName = CFCAdverts.HOOK_BASE .. "_" .. advertName
+        CFCNotifications.new( notifName, advert.type, true )
+        local notif = CFCNotifications.get( notifName )
+        
+        notif:SetTitle( advert.title )
+        notif:SetText( advert.text )
+        notif:SetTextColor( advert.textColor )
+        notif:SetDisplayTime( advert.displayTime )
+        notif:SetPriority( advert.priority )
+        notif:SetCloseable( advert.closeable )
+        notif:SetIgnoreable( advert.ignoreable )
+        notif:SetTimed( advert.timed )
+        
+        if advert.type == "Buttons" then
+            for i, button in pairs( advert.buttons ) do
+                notif:AddButton( button.text, button.buttonColor, i )
+            end
+            
+            function notif:OnButtonPressed( ply, index, ... )
+                local button = advert.buttons[index]
+                
+                button.func( ply, arg )
+                
+                if not button.closeOnPress then return end
+                
+                notif:RemovePopup( notif:GetCallingPopupID(), ply )
+            end
+        end
+        
+        notif:Send( plys )
+        
+        curAdvert = curAdvert % advertCount + 1
+    end )
+end
+
+--Start adverting once the server is fully loaded or done changing levels
+hook.Add( "InitPostEntity", CFCAdverts.HOOK_NAME .. "_StartAdverting", function()
+    timer.Remove( timerName )
+    timer.Simple( 30, CFCAdverts.updateAdverts )
+end )
