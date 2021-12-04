cd8d4490e4506c4003df92744ff879b74ce3e8d6
legokidlogan <legokidlogan@gmail.com>
2021-12-03 17:19:58 -0700

    Allow players to block advert groups

--- lua/cfc_adverts/client/cl_net.lua
+++ lua/cfc_adverts/client/cl_net.lua
@@ -2,3 +2,23 @@ net.Receive( CFCAdverts.HOOK_URL, function()
     chat.Close()
     gui.OpenURL( net.ReadString() )
 end )
+
+net.Receive( CFCAdverts.HOOK_IGNORE_GROUP, function()
+    if not CFCNotifications then return end
+
+    local groupCount = net.ReadInt( 10 )
+    local permIgnores = CFCNotifications._permIgnores
+    local tempIgnores = CFCNotifications._tempIgnores
+
+    for i = 1, groupCount do
+        local notifID = net.ReadString()
+
+        CFCNotifications._removePopupByNotificationID( notifID )
+
+        permIgnores[notifID] = true
+        tempIgnores[notifID] = nil
+    end
+
+    CFCNotifications.saveIgnores()
+    CFCNotifications._reloadIgnoredPanels()
+end )
