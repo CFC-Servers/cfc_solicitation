c4290f8dc160284f97d55c55e4d82884e7e6658e
legokidlogan <legokidlogan@gmail.com>
2022-06-22 15:00:32 -0700

    Add ignore print message

--- lua/cfc_adverts/client/cl_net.lua
+++ lua/cfc_adverts/client/cl_net.lua
@@ -1,3 +1,7 @@
+local COLOR_GREY = Color( 150, 150, 150, 255 )
+local COLOR_DARK_GREY = Color( 41, 41, 41, 255 )
+local COLOR_HIGHLIGHT = Color( 255, 230, 75, 255 )
+
 net.Receive( CFCAdverts.HOOK_URL, function()
     chat.Close()
     gui.OpenURL( net.ReadString() )
@@ -7,6 +11,7 @@ net.Receive( CFCAdverts.HOOK_IGNORE_GROUP, function()
     if not CFCNotifications then return end
 
     local groupCount = net.ReadInt( 10 )
+    local groupName = net.ReadString()
     local permIgnores = CFCNotifications._permIgnores
     local tempIgnores = CFCNotifications._tempIgnores
 
@@ -21,4 +26,13 @@ net.Receive( CFCAdverts.HOOK_IGNORE_GROUP, function()
 
     CFCNotifications.saveIgnores()
     CFCNotifications._reloadIgnoredPanels()
+
+    chat.AddText(
+        COLOR_DARK_GREY, "[",
+        COLOR_GREY, "Notifications",
+        COLOR_DARK_GREY, "] ",
+        color_white, "You will never see notifications from the ",
+        COLOR_HIGHLIGHT, groupName,
+        color_white, " group again."
+    )
 end )
